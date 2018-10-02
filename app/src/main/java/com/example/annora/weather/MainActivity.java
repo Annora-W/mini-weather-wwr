7530311778fd6adf67893eadd158df6254e38cdd
WangWanrong <wangwr@pku.edu.cn>
2018-10-02 10:39:04 +0800

    顶部工具栏

---
+++ app/src/main/java/com/example/annora/weather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.annora.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);//在Activity中指定布局文件
+
+    }
+}
